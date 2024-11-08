4fd9369d423e7be89d0cce08725b725dd15de3ef
Edmund Ochieng <ochienged@gmail.com>
2024-11-08 03:05:12 -0600

    Add support for OCI registry helm charts
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

--- internal/operator-controller/rukpak/source/tgz.go
+++ internal/operator-controller/rukpak/source/tgz.go
@@ -2,10 +2,10 @@ package source
 
 import (
 	"archive/tar"
+	"bytes"
 	"compress/gzip"
 	"context"
 	"crypto/tls"
-	"crypto/x509"
 	"errors"
 	"fmt"
 	"io"
@@ -16,9 +16,6 @@ import (
 	"path/filepath"
 	"strings"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -26,68 +23,6 @@ import (
 
 type TarGZ struct {
 	BaseCachePath string
-}
-
-// downloader uses a custom HTTP client that accepts a URL and returns a HTTP response and an error
-// The client would be aware of the user-generated CA certificates
-func downloader(ctx context.Context, url string) (*http.Response, error) {
-	// Get a kubernetes client
-	client, err := kubeClient()
-	if err != nil {
-		return nil, fmt.Errorf("getting k8s client; %w", err)
-	}
-
-	// Retrieve the contents of the tls secret associated with cert-manager olmv1-ca certificate object
-	secret, err := client.CoreV1().Secrets("olmv1-system").Get(ctx, "olmv1-cert", metav1.GetOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("retrieving ca cert secret; %w", err)
-	}
-
-	// Create a PEM certificate using the contents of the tls secret
-	caPEM := []byte{}
-	for _, v := range secret.Data {
-		caPEM = append(caPEM[:], v[:]...)
-	}
-
-	// Append the PEM certificate to the system certificate pool
-	caPool, err := x509.SystemCertPool()
-	if err != nil {
-		return nil, fmt.Errorf("system certificate pool; %w", err)
-	}
-
-	if ok := caPool.AppendCertsFromPEM(caPEM); !ok {
-		return nil, errors.New("error creating PEM encoded certificate")
-	}
-
-	// Create a custom HTTP client that will use the new certificate pool
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: false,
-				RootCAs:            caPool,
-			},
-		},
-	}
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return httpClient.Do(req)
-}
-
-func kubeClient() (*kubernetes.Clientset, error) {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-
-	return clientset, nil
 }
 
 func (i *TarGZ) Unpack(ctx context.Context, bundle *BundleSource) (*Result, error) {
@@ -103,20 +38,36 @@ func (i *TarGZ) Unpack(ctx context.Context, bundle *BundleSource) (*Result, erro
 		return nil, reconcile.TerminalError(fmt.Errorf("error downloading bundle '%s': %v", bundle.Name, err))
 	}
 	fileName := path.Base(parsedURL.Path)
+	if strings.Contains(fileName, ":") {
+		s := strings.Split(fileName, ":")
+		fileName = fmt.Sprintf("%s-%s.tgz", s[0], s[1])
+	}
+
+	// Append OLMv1 CA certificate file in PEM format to the system certificate pool
+	certPool, err := buildCertPool(ctx, "olmv1-cert", "olmv1-system")
+
+	// Create a custom HTTP client that will use the new certificate pool
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: err != nil,
+				RootCAs:            certPool,
+			},
+		},
+	}
 
 	// Download the .tgz file
-	resp, err := downloader(ctx, bundle.Image.Ref)
+	downloader, err := newChartDownloader(bundle.Image.Ref)
+	if err != nil {
+		return nil, reconcile.TerminalError(fmt.Errorf("error setting up chart downloader: %v", err))
+	}
+	content, err := downloader.Download(ctx, httpClient)
 	if err != nil {
 		return nil, reconcile.TerminalError(fmt.Errorf("error downloading bundle '%s': %v", bundle.Name, err))
 	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, reconcile.TerminalError(fmt.Errorf("error downloading bundle '%s': got status code '%d'", bundle.Name, resp.StatusCode))
-	}
 
 	// Open a gzip reader
-	gzReader, err := gzip.NewReader(resp.Body)
+	gzReader, err := gzip.NewReader(bytes.NewReader(content))
 	if err != nil {
 		return nil, reconcile.TerminalError(fmt.Errorf("error unpacking bundle '%s': %v", bundle.Name, err))
 	}
