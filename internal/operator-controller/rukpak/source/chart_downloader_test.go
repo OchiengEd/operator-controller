7048ac81b03f8be4add9127bb4a50fc175190ffb
Edmund Ochieng <ochienged@gmail.com>
2024-11-08 03:05:12 -0600

    Add support for OCI registry helm charts
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

---
+++ internal/operator-controller/rukpak/source/chart_downloader_test.go
@@ -0,0 +1,62 @@
+package source
+
+import (
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func Test_chartDownloader(t *testing.T) {
+	testcases := []struct {
+		name    string
+		url     string
+		wantMd5 string
+		wantErr bool
+	}{
+		{
+			name:    "Download chart from OCI registry",
+			url:     "oci://localhost:5000/charts/metrics-server:3.12.0",
+			wantErr: false,
+			wantMd5: "9e68a30ac986aab2aa7ad20187125e44",
+		},
+		{
+			name:    "Download chart from HTTP registry",
+			url:     "https://github.com/kubernetes-sigs/metrics-server/releases/download/metrics-server-helm-chart-3.12.0/metrics-server-3.12.0.tgz",
+			wantErr: false,
+			wantMd5: "9e68a30ac986aab2aa7ad20187125e44",
+		},
+	}
+
+	ctx := context.Background()
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			downloader, _ := newChartDownloader(tc.url)
+			got, err := downloader.Download(ctx, &http.Client{})
+			if (err != nil) != tc.wantErr {
+				t.Errorf("error downloading chart; %v", err)
+				return
+			}
+			hash := md5.Sum(got)
+			checksum := hex.EncodeToString(hash[:])
+			fileName := filepath.Base(tc.url)
+			if strings.Contains(fileName, ":") {
+				s := strings.Split(fileName, ":")
+				fileName = fmt.Sprintf("%s-%s.tgz", s[0], s[1])
+			}
+			t.Logf("Filename %s\n", fileName)
+
+			if checksum != tc.wantMd5 {
+				t.Errorf("The md5 sum for %s is %s but, was expecting %s",
+					fileName,
+					checksum,
+					tc.wantMd5,
+				)
+			}
+		})
+	}
+}
